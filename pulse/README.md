5cbc9abe0391fac3f9a473c2ac9544584517f5f9
hbc <bcxxxxxx@gmail.com>
2016-12-25 21:35:17 +0800

    feat(pulse): implement metric model

---
+++ pulse/README.md
@@ -0,0 +1,9 @@
+# pulse
+
+Repository pulse... with more power.
+
+## Usage
+
+```
+$ ./pulse
+```
---
+++ pulse/metric/model.go
@@ -0,0 +1,28 @@
+package metric
+
+import "time"
+
+type User struct {
+	Login string `json:"login"`
+	Name  string `json:"name"`
+}
+
+type PullRequest struct {
+	ID           string    `json:"id"`
+	Number       string    `json:"number"`
+	URL          string    `json:"url"`
+	State        string    `json:"state"`
+	Title        string    `json:"title"`
+	Body         string    `json:"body"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	ClosedAt     time.Time `json:"closed_at"`
+	MergedAt     time.Time `json:"merged_at"`
+	Additions    int       `json:"additions"`
+	Deletions    int       `json:"deletions"`
+	ChangedFiles int       `json:"changed_files"`
+
+	User      *User   `json:"user"`
+	MergedBy  *User   `json:"merged_by"`
+	Assignees []*User `json:"assignees"`
+}
